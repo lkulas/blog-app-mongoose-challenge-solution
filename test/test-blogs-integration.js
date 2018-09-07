6d50786664b83e0ad3cbeacce34b88b7a3db1eb1
Laura Kulas <laura.kulas@gmail.com>
2018-09-06 19:49:58 -0700

    added function to seed blog data

--- test/test-blogs-integration.js
+++ test/test-blogs-integration.js
@@ -10,3 +10,13 @@ const expect = chai.expect;
 const { BlogPost } = require('../models');
 const { app, runServer, closeServer } = require('../server');
 const { TEST_DATABASE_URL } = require('../config');
+
+chai.use(chaiHttp);
+
+function seedBlogData() {
+	const seedData = [];
+	for (let i = 1, i <= 10; i++) {
+		seedData.push(generateBlogData());
+	}
+	return BlogPost.insertMany(seedData);
+}
