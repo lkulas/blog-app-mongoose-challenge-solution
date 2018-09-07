23c9568d8e122a52d75fbbc97a8f4f64004a09a4
Laura Kulas <laura.kulas@gmail.com>
2018-09-06 20:07:07 -0700

    added test for GET endpoint

--- test/test-blogs-integration.js
+++ test/test-blogs-integration.js
@@ -14,6 +14,7 @@ const { TEST_DATABASE_URL } = require('../config');
 chai.use(chaiHttp);
 
 function seedBlogData() {
+	console.log('Seeding database');
 	const seedData = [];
 	for (let i = 1, i <= 10; i++) {
 		seedData.push(generateBlogData());
@@ -32,3 +33,41 @@ function generateBlogData() {
 		created: faker.date.past()
 	};
 };
+
+function tearDownDb() {
+	console.warn('Deleting database');
+	return mongoose.connection.dropDatabase();
+};
+
+describe('BlogPosts API resource', function() {
+	before(function() {
+		return runServer(TEST_DATABASE_URL);
+	});
+	beforeEach(function() {
+		return seedBlogData();
+	});
+	afterEach(function() {
+		return tearDownDb();
+	});
+	after(function() {
+		return closeServer();
+	});
+});
+
+describe('GET endpoint', function() {
+	it('should return all existing blog posts', function() {
+		let res;
+		return chai.request(app)
+			.get('/posts')
+			.then(function(_res) {
+				res = _res;
+				expect(res).to.have.status(200);
+				expect(res.body.posts).to.have.lengthOf.at.least(1);
+				return BlogPost.count();
+			})
+			.then(function(count) {
+				expect(res.body.posts).to.have.lengthOf(count);
+			});
+	});
+})
+
