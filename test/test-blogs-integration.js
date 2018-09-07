79db86c208d9856192debf29f008147627383eb8
Laura Kulas <laura.kulas@gmail.com>
2018-09-07 12:12:42 -0700

    completed test for GET endpoint

--- test/test-blogs-integration.js
+++ test/test-blogs-integration.js
@@ -14,13 +14,13 @@ const { TEST_DATABASE_URL } = require('../config');
 chai.use(chaiHttp);
 
 function seedBlogData() {
-	console.log('Seeding database');
+	console.info('Seeding database');
 	const seedData = [];
-	for (let i = 1, i <= 10; i++) {
+	for (let i=1; i<=10; i++) {
 		seedData.push(generateBlogData());
 	}
 	return BlogPost.insertMany(seedData);
-};
+}
 
 function generateBlogData() {
 	return {
@@ -32,12 +32,12 @@ function generateBlogData() {
 		},
 		created: faker.date.past()
 	};
-};
+}
 
 function tearDownDb() {
 	console.warn('Deleting database');
 	return mongoose.connection.dropDatabase();
-};
+}
 
 describe('BlogPosts API resource', function() {
 	before(function() {
@@ -52,22 +52,45 @@ describe('BlogPosts API resource', function() {
 	after(function() {
 		return closeServer();
 	});
-});
 
-describe('GET endpoint', function() {
-	it('should return all existing blog posts', function() {
-		let res;
-		return chai.request(app)
-			.get('/posts')
-			.then(function(_res) {
-				res = _res;
-				expect(res).to.have.status(200);
-				expect(res.body.posts).to.have.lengthOf.at.least(1);
-				return BlogPost.count();
-			})
-			.then(function(count) {
-				expect(res.body.posts).to.have.lengthOf(count);
-			});
+	describe('GET endpoint', function() {
+		it('should return all existing blog posts', function() {
+			let res;
+			return chai.request(app)
+				.get('/posts')
+				.then(function(_res) {
+					res = _res;
+					expect(res).to.have.status(200);
+					expect(res.body).to.have.lengthOf.at.least(1);
+					return BlogPost.count();
+				})
+				.then(function(count) {
+					expect(res.body).to.have.lengthOf(count);
+				});
+		});
+		it('should return posts with correct fields', function() {
+			let resBlogPost;
+			return chai.request(app)
+				.get('/posts')
+				.then(function(res) {
+					expect(res).to.have.status(200);
+					expect(res).to.be.json;
+					expect(res.body).to.be.a('array');
+					expect(res.body).to.have.lengthOf.at.least(1);
+					res.body.forEach(function(post) {
+						expect(post).to.be.a('object');
+						expect(post).to.include.keys(
+							'id', 'title', 'content', 'author', 'created');
+					});
+					resBlogPost = res.body[0];
+					return BlogPost.findById(resBlogPost.id);
+				})
+				.then(function(post) {
+					expect(resBlogPost.id).to.equal(post.id);
+					expect(resBlogPost.title).to.equal(post.title);
+					expect(resBlogPost.content).to.equal(post.content);
+					expect(resBlogPost.author).to.contain(post.author.firstName, post.author.lastName);
+				});
+		});
 	});
-})
-
+});
