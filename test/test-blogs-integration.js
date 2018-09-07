28264d47af896fc81cd049560b72f4eb41cb708e
Laura Kulas <laura.kulas@gmail.com>
2018-09-07 12:49:36 -0700

    added test for DELETE endpoint

--- test/test-blogs-integration.js
+++ test/test-blogs-integration.js
@@ -150,4 +150,24 @@ describe('BlogPosts API resource', function() {
 				});
 		});
 	});
+
+	describe('DELETE endpoint', function() {
+		it('should delete a post by id', function() {
+			let post;
+			return BlogPost
+				.findOne()
+				.then(function(_post) {
+					post = _post;
+					return chai.request(app)
+						.delete(`/posts/${post.id}`);
+				})
+				.then(function(res) {
+					expect(res).to.have.status(204);
+					return BlogPost.findById(post.id);
+				})
+				.then(function(_post) {
+					expect(_post).to.be.null;
+				});
+		});
+	});
 });
